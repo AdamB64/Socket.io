f29ecc022ec5f8602cb159809e4b0ea4d859e748
dcorsar <davidcorsar@gmail.com>
2022-03-10 22:15:40 +0000

    fully working code

---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+var socket = io();
+
+$('#form').submit(function () {
+   // insert emitting message code here
+   var message = $('#input').val();
+   var username = $('#username').val();
+   if (message && username) {
+        socket.emit('chat message',{ "message":message, "username":username });
+       $('#input').val("");
+   }
+   return false;
+});
+
+socket.on('chat message', function(msg){
+    // insert code for displaying msg
+    $('#messages').append("<li>" + msg.username + ": " + msg.message + "</li>");
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+
+// {user:$("#name").val(), message:message}
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+//code to define the public "static" folder
+app.use(express.static('public'))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+})
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    // add disconnect code here
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    // add message handling code here
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg)
+        io.emit('chat message', msg);
+    });
+});
+
+
+//       
